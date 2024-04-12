30bab1ae901b833d4b4e4279c08f9e3c0d0ca95a
Michael Grissinger <brucewyne@gmail.com>
2024-04-11 22:18:30 -0400

    add publicly_accessible

--- terraform/dev/db.tf
+++ terraform/dev/db.tf
@@ -35,6 +35,7 @@ resource "aws_db_instance" "dev_database" {
   username             = "dbadmin"
   password             = "adminpassword" //change this after, or load it from a GH secret
   parameter_group_name = "default.mysql8.0"
+  publicly_accessible  = true
   skip_final_snapshot  = true
   db_subnet_group_name = aws_db_subnet_group.main.name
   vpc_security_group_ids = [aws_security_group.allow_all.id]
